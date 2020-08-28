e59aae8db95e25fca2d700f4f958e9e3015fe10f
marissa-mayes <marissa.mayes1289@gmail.com>
2020-08-27 21:57:27 -0500

    corrected delete error

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,25 @@
 const orm = require("../config/orm.js");
 let burger = {
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
-      });
-    },
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-          cb(res);
-        });
-      },    
-     updateOne: function(cols,vals, cb) {
-        orm.updateOne("burgers", cols, vals, function(res) {
-          cb(res);
-        });
-      },  
-    }  
-    module.exports= burger;
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (cols, vals, cb) {
+    orm.updateOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  delete: function (condition,cb){
+    orm.delete("burgers",condition, function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
